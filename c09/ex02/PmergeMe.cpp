d1e03b8dc154051e2cdaae076cb8ace8ebdffff6
hoone0802 <sabyun@student.42seoul.kr>
2024-07-11 16:40:12 +0900

    add end point sort

--- c09/ex02/PmergeMe.cpp
+++ c09/ex02/PmergeMe.cpp
@@ -59,42 +59,47 @@ std::vector<std::pair<int, int> > PmergeMe::req(std::vector<std::pair<int, int> 
 		arr[i].first <= arr[i+1].first ? lage_num.push_back(arr[i+1]) : small_num.push_back(arr[i+1]);
 	}
 	if(arr.size()%2)
-	{
-		// std::cout <<" n = " << n << "  remaind " << arr[arr.size()-1].first << std:: endl;
 		small_num.push_back(arr[arr.size()-1]);
-	}
 	lage_num =req(lage_num,n+1);
-	int n_pow = std::pow(2,n);
-	for(int i = 0; i < (int)lage_num.size(); i++)
+	int n_pow = std::pow(2, n);
+	for (int i = 0; i < (int)lage_num.size(); i++)
 		temp.push_back(small_num[lage_num[i].second / n_pow]);
-	if(arr.size()%2)
-		temp.push_back(small_num[small_num.size()-1]);
-	int k =0;
-    for (int i = 1; i < 19; i++) {
-        long long ya_curr = std::min((int)find_ya(i),(int)lage_num.size());
-        long long ya_prev = find_ya(i - 1);
-        for (long long j = ya_curr; j > ya_prev; j--) {
-            if (j + k > ((long long)lage_num.size()))
-                continue;
-            unsigned int insert_index = (unsigned int)(j - 1);
-            if (insert_index >= temp.size())
-                continue;
-            std::vector<std::pair<int, int> >::iterator it;
-			it = std::lower_bound(lage_num.begin(),lage_num.begin() + j + k ,temp[insert_index]);
-			std::cout << "n = " << n << "lage size = " << lage_num.size() << " j = " << j << " small " << temp[insert_index].first << " ya_pre " << ya_prev << " k = " << k  << "2^n-1 = " << n_pow -1 << std::endl;
-            lage_num.insert(it, temp[insert_index]);
-			// for(int i = 0; i < (int)lage_num.size(); i++)
-			// 	std::cout << lage_num[i].first << std::endl;
-			k++;
-        }
-		if((int)(temp.size()) < (int)find_ya(i))
-			break;
-	}
 	if (arr.size() % 2)
+		temp.push_back(small_num[small_num.size() - 1]);
+	std::cout << "temp list " << std::endl;
+	for (unsigned int i = 0; i < temp.size(); i++)
+		std::cout << temp[i].first << std::endl;
+
+	std::cout << "lage list " << std::endl;
+	for (unsigned int i = 0; i < lage_num.size(); i++)
+		std::cout << lage_num[i].first << std::endl;
+	int k = 0;
+	for (int i = 1; i < 19; i++)
 	{
-		std::vector<std::pair<int, int> >::iterator it;
-		it = std::lower_bound(lage_num.begin(), lage_num.end(), temp[temp.size() - 1]);
-		lage_num.insert(it, temp[temp.size() - 1]);
+		long long ya_curr = std::min((int)find_ya(i), (int)(temp.size() > lage_num.size() ? temp.size() : lage_num.size()));
+		long long ya_prev = find_ya(i - 1);
+		for (long long j = ya_curr; j > ya_prev; j--)
+		{
+			if (j + k > ((long long)lage_num.size()))
+			{
+				if (j - 1 == (int)temp.size() - 1)
+				{
+					std::vector<std::pair<int, int>>::iterator it;
+					it = std::lower_bound(lage_num.begin(), lage_num.end(), temp[j - 1]);
+					lage_num.insert(it, temp[j - 1]);
+				}
+				continue;
+			}
+			unsigned int insert_index = (unsigned int)(j - 1);
+			if (insert_index > temp.size() - 1)
+				continue;
+			std::vector<std::pair<int, int>>::iterator it;
+			it = std::lower_bound(lage_num.begin(), lage_num.begin() + j + k, temp[insert_index]);
+			lage_num.insert(it, temp[insert_index]);
+			k++;
+		}
+		if ((int)(temp.size()) < (int)find_ya(i))
+			break;
 	}
 	return lage_num;
 }
